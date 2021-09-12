7d839754ac4cc8c1aa7a98fb45edc586f51d2c81
Akande Benji <adeoul001@gmail.com>
2021-09-12 13:10:52 -0700

    Added active field for delete req

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require("../model/userModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/AppError");
+
 
 const factory = require("./handlerFactory");
 
@@ -34,3 +36,15 @@ exports.userStats =  catchAsync(async (req, res,next) => {
     });
   });
 
+exports.getUserByUsername = catchAsync(async (req, res, next) => {
+  const document = await User.findOne({username:req.params.username});
+  if (!document) {
+    return next(new AppError("document Not Found with This Id", 404));
+  }
+  res.status(200).json({
+    message: "success",
+    data: {
+      document,
+    },
+  });
+});
